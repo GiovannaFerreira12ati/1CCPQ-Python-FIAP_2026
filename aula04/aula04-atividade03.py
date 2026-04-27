a97a402686c715dacaf8819e489652898108a22f
GiovannaFerreira12ati <giovannafa1308@gmail.com>
2026-04-27 20:39:10 -0300

    aula 27 abril

---
+++ aula04/aula04-atividade03.py
@@ -0,0 +1,19 @@
+# for count_music in range (3):
+#     print(f"Musica {count_music}")
+#
+#     #de um ate 11, pulando de 2 em 2
+# for i in range (1, 12, 2):
+#     print(i)
+
+
+    #ATV3
+# qtd_musicas = int(input("Digite a qtd de músicas:"))
+#
+# for i in range(qtd_musicas):
+#     print(f"Música {i}")
+
+#LACOS ANINHADOS
+#REP. ENCADEADA
+for i in range (0, 4):
+    for j in range(0,3,2):
+        print(f"i:{i}, j:{j}")
---
+++ aula04/aula04-while.py
@@ -0,0 +1,10 @@
+cp = 0
+while cp < 3:
+    print(f"Produto {cp}")
+    cp+=1
+
+#While decrescente de 4 até 1
+i=4
+while i > 0:
+    print(i)
+    i -= 1
